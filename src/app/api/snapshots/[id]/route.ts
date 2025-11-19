4cb184a5c6d994456e405ceb4191892254ec3b12
Bruno <brunofreire1717@gmail.com>
2025-11-18 22:01:00 -0500

    chore: in snapshot

--- src/app/api/snapshots/[id]/route.ts
+++ src/app/api/snapshots/[id]/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/backend/prisma";
 
-export async function GET(request: Request, context: { params: { id: string }}) {
+export async function GET(request: Request, context: { params: { id: string } }) {
   try {
     const { id } = context.params;
     const snapshot = await prisma.list.findUnique({
